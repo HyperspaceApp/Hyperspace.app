f031e678309392ffaa62f9f14a093ad46bc72924
Mark Huetsch <mark@hspace.app>
2018-07-16 23:15:54 +0800

    fix release on macos

--- release.sh
+++ release.sh
@@ -35,9 +35,12 @@ else
 	pubkeyFile=`perl -e 'use Cwd "abs_path";print abs_path(shift)' $2`
 fi
 
-electronOSX="https://github.com/electron/electron/releases/download/${electronVersion}/electron-${electronVersion}-darwin-x64.zip"
-electronLinux="https://github.com/electron/electron/releases/download/${electronVersion}/electron-${electronVersion}-linux-x64.zip"
-electronWindows="https://github.com/electron/electron/releases/download/${electronVersion}/electron-${electronVersion}-win32-x64.zip"
+electronOSXZip="electron-${electronVersion}-darwin-x64.zip"
+electronOSX="https://github.com/electron/electron/releases/download/${electronVersion}/${electronOSXZip}"
+electronLinuxZip="electron-${electronVersion}-linux-x64.zip"
+electronLinux="https://github.com/electron/electron/releases/download/${electronVersion}/${electronLinuxZip}"
+electronWindowsZip="electron-${electronVersion}-win32-x64.zip"
+electronWindows="https://github.com/electron/electron/releases/download/${electronVersion}/${electronWindowsZip}"
 
 hyperspaceOSX="/Users/mark/.go/src/github.com/HyperspaceApp/Hyperspace/release/Hyperspace-${hyperspaceVersion}-darwin-amd64.zip"
 hyperspaceLinux="/Users/mark/.go/src/github.com/HyperspaceApp/Hyperspace/release/Hyperspace-${hyperspaceVersion}-linux-amd64.zip"
@@ -50,20 +53,31 @@ mkdir -p release/{osx,linux,win32}
 package() {
 	src=$1
 	dest=$2
-	cp -r ${src}/{plugins,assets,css,dist,index.html,package.json,js} $dest
+	cp -r ${src}/{plugins,assets,css,dist,app.html,package.json,js} $dest
 }
 
 buildOSX() {
+	if [ ! -f $electronOSXZip ]; then
+		wget $electronOSX
+	fi
+	cp $electronOSXZip release/osx/$electronOSXZip
 	cd release/osx
-	wget $electronOSX
-	unzip ./electron*
+	unzip ./$electronOSXZip
 	mv Electron.app Hyperspace.app
 	mv Hyperspace.app/Contents/MacOS/Electron Hyperspace.app/Contents/MacOS/Hyperspace
-	# NOTE: this only works with GNU sed, other platforms (like OSX) may fail here
-	sed -i 's/>Electron</>Hyperspace</' Hyperspace.app/Contents/Info.plist
-	sed -i 's/>'"${electronVersion:1}"'</>'"${hyperspaceVersion:1}"'</' Hyperspace.app/Contents/Info.plist
-	sed -i 's/>com.github.electron\</>com.hyperspace.hyperspaceapp</' Hyperspace.app/Contents/Info.plist
-	sed -i 's/>electron.icns</>icon.icns</' Hyperspace.app/Contents/Info.plist
+	# NOTE: this code relies on GNU sed, other platforms (like OSX, which has a weird sed) will fail here.
+	# Therefore we use gnu-sed on MacOS (brew install gnu-sed)
+	if [ "$(uname -s)" = 'Darwin' ]; then
+		gsed -i 's/>Electron</>Hyperspace</' Hyperspace.app/Contents/Info.plist
+		gsed -i 's/>'"${electronVersion:1}"'</>'"${hyperspaceVersion:1}"'</' Hyperspace.app/Contents/Info.plist
+		gsed -i 's/>com.github.electron\</>com.hyperspace.hyperspaceapp</' Hyperspace.app/Contents/Info.plist
+		gsed -i 's/>electron.icns</>icon.icns</' Hyperspace.app/Contents/Info.plist
+	else
+		sed -i 's/>Electron</>Hyperspace</' Hyperspace.app/Contents/Info.plist
+		sed -i 's/>'"${electronVersion:1}"'</>'"${hyperspaceVersion:1}"'</' Hyperspace.app/Contents/Info.plist
+		sed -i 's/>com.github.electron\</>com.hyperspace.hyperspaceapp</' Hyperspace.app/Contents/Info.plist
+		sed -i 's/>electron.icns</>icon.icns</' Hyperspace.app/Contents/Info.plist
+	fi
 	cp ../../assets/icon.icns Hyperspace.app/Contents/Resources/
 	rm -r Hyperspace.app/Contents/Resources/default_app.asar
 	mkdir Hyperspace.app/Contents/Resources/app
@@ -75,14 +89,17 @@ buildOSX() {
 		mv ./Hyperspace-* ./Hyperspace
 	)
 	package "../../" "Hyperspace.app/Contents/Resources/app"
-	rm -r electron*.zip
+	rm -r $electronOSXZip
 	cp ../../LICENSE .
 }
 
 buildLinux() {
+	if [ ! -f $electronLinuxZip ]; then
+		wget $electronLinux
+	fi
+	cp $electronLinuxZip release/linux/$electronLinuxZip
 	cd release/linux
-	wget $electronLinux
-	unzip ./electron*
+	unzip $electronLinuxZip
 	mv electron Hyperspace
 	rm -r resources/default_app.asar
 	mkdir resources/app
@@ -94,16 +111,20 @@ buildLinux() {
 		mv ./Hyperspace-* ./Hyperspace
 	)
 	package "../../" "resources/app"
-	rm -r electron*.zip
+	rm -r $electronLinuxZip
 	cp ../../LICENSE .
 }
 
 buildWindows() {
+	if [ ! -f $electronWindowsZip ]; then
+		wget $electronWindows
+	fi
+	cp $electronWindowsZip release/win32/$electronWindowsZip
 	cd release/win32
-	wget $electronWindows
-	unzip ./electron*
+	unzip $electronWindowsZip
 	mv electron.exe Hyperspace.exe
 	wget https://github.com/electron/rcedit/releases/download/v0.1.0/rcedit.exe
+	# NOTE the wine part needs to be handled manually right now
 	wine rcedit.exe Hyperspace.exe --set-icon '../../assets/icon.ico'
 	rm -f rcedit.exe
 	rm resources/default_app.asar
@@ -116,7 +137,7 @@ buildWindows() {
 		mv ./Hyperspace-* ./Hyperspace
 	)
 	package "../../" "resources/app"
-	rm -r electron*.zip
+	rm -r $electronWindowsZip
 	cp ../../LICENSE .
 }
 
@@ -130,7 +151,7 @@ buildWindows() {
 ( buildWindows )
 
 # make signed zip archives for each release
-for os in win32 linux osx; do 
+for os in win32 linux osx; do
 	(
 		cd release/${os}
 		zip -r ../Hyperspace-${uiVersion}-${os}-x64.zip .
