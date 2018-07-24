93bc2e74b8ce939ed1c94096aa0ca9d10a6f6ecc
Mark Huetsch <mark@hspace.app>
2018-07-24 11:52:33 +0800

    formatting

--- plugins/Wallet/js/components/lockscreen.js
+++ plugins/Wallet/js/components/lockscreen.js
@@ -4,33 +4,21 @@ import PasswordPrompt from '../containers/passwordprompt.js'
 import UninitializedWalletDialog from '../containers/uninitializedwalletdialog.js'
 import RescanDialog from './rescandialog.js'
 
-const LockScreen = ({unlocked, unlocking, encrypted, rescanning}) => {
+const LockScreen = ({ unlocked, unlocking, encrypted, rescanning }) => {
 	if (unlocked && encrypted && !unlocking && !rescanning) {
 		// Wallet is unlocked and encrypted, return an empty lock screen.
-		return (
-			<div />
-		)
+		return <div />
 	}
 	let lockscreenContents
 	if (!unlocked && encrypted && !rescanning) {
-		lockscreenContents = (
-			<PasswordPrompt />
-		)
+		lockscreenContents = <PasswordPrompt />
 	} else if (rescanning) {
-		lockscreenContents = (
-			<RescanDialog />
-		)
+		lockscreenContents = <RescanDialog />
 	} else if (!encrypted) {
 		// Wallet is not encrypted, return a lockScreen that initializes a new wallet.
-		lockscreenContents = (
-			<UninitializedWalletDialog />
-		)
+		lockscreenContents = <UninitializedWalletDialog />
 	}
-	return (
-		<div className="lockscreen">
-			{lockscreenContents}
-		</div>
-	)
+	return <div className="lockscreen">{lockscreenContents}</div>
 }
 LockScreen.propTypes = {
 	unlocked: PropTypes.bool.isRequired,
