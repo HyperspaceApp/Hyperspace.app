b7fe3bf514374f6ec72d85de697321505b889ef0
Mark Huetsch <mark@hspace.app>
2018-07-25 19:41:21 -0500

    bump versions

--- release.sh
+++ release.sh
@@ -22,8 +22,8 @@ npm install
 rm -rf ./dist
 npm run build
 
-uiVersion=${3:-v0.0.1}
-hyperspaceVersion=${4:-v0.0.1}
+uiVersion=${3:-v0.1.0}
+hyperspaceVersion=${4:-v0.1.0}
 electronVersion=${5:-v2.0.2}
 
 # fourth argument is the public key file path.
